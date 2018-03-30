e2f44ac0fabc4296353167ae51e8f4eb1df605f2
wangzhaoxin <wangzhaoxin@syswin.com>
2018-03-30 11:10:15 +0800

    修改注释

--- app/src/main/java/com/xin/framework/xinframwork/hybrid/webview/IWebViewInit.java
+++ app/src/main/java/com/xin/framework/xinframwork/hybrid/webview/IWebViewInit.java
@@ -7,12 +7,7 @@ import android.webkit.WebView;
 import java.io.File;
 
 /**
- * Description :
  * Created by 王照鑫 on 2017/11/1 0001.
- * Job number：147109
- * Email： wangzhaoxin@syswin.com
- * Person in charge :
- * Leader：guohaichun
  */
 
 public interface IWebViewInit {
